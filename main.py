97135d922f6d806713fd119a5af12f3222430579
David.md <david-linus.rutkevich@ueg-leer.net>
2023-01-03 00:15:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from apps.cbc import *
 
+icon = Image.open("style/favicon.ico")
 st.set_page_config(
     page_title="CellAlyse",
-    page_icon=":microscope:",
+    page_icon=icon,
     layout="centered",
     initial_sidebar_state="expanded",
 )
 # use option_menu instead of steamlit mutli page
-
+def clean():
+    # remove all files in tmp folder
+    for file in os.listdir("storage/tmp"):
+        os.remove(os.path.join("storage/tmp", file))
 
 hide_streamlit_style = """
                 <style>
@@ -47,11 +51,21 @@ with st.sidebar:
     selected = option_menu("Navigation", ["Home", "weiße Blutzellen", "Zählung"],
                            icons=["house", "search", "clipboard-data"],
                            styles={
-                               "icon": {"color": "#f9f871", "font-size": "20px"},
-                               "nav-link-selected": {"background-color": "#807286"},
-                               "nav-link": {"background-color": "#0e1117", "font-size": "19px"},
+                               "icon": {"color": "#ffffff", "font-size": "20px"},
+                               "nav-link-selected": {"background-color": "#9a57a3"},
                            })
 
     # create a multi-page app the apps are in the apps folder
 if selected == "Zählung":
     cbc()
+    clean()
+
+
+
+
+
+
+
+
+
+
