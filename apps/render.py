eb26ffbf4ee80d23e898f5c4058f42b449be6516
David.md <david-linus.rutkevich@ueg-leer.net>
2023-01-30 16:15:39 +0100

    Update render.py

--- apps/render.py
+++ apps/render.py
@@ -13,7 +13,6 @@ frontend_dir = (Path(__file__).parent / "frontend").absolute()
 
 _component_func = components.declare_component("stpyvista", path=str(frontend_dir))
 
-@st.experimental_memo(suppress_st_warning=True)
 def render():
     pv.set_jupyter_backend("pythreejs")
 
